637f5cb3eb6eda7f39c53501194ec5c54f6f62b8
pezholio <pezholio@gmail.com>
2015-01-16 17:11:38 +0000

    Match postcode to last element in regex
    It will always be at the end of a string

--- lib/sorting_office/models/postcode.rb
+++ lib/sorting_office/models/postcode.rb
@@ -3,7 +3,7 @@ class Postcode
   REGEX = /([A-PR-UWYZ01][A-Z01]?[0-9IO][0-9A-HJKMNPR-YIO]?\s?[0-9IO][ABD-HJLNPQ-Z10]{2})/i
 
   def self.calculate(address)
-    postcode = UKPostcode.new(address.match(REGEX)[0])
+    postcode = UKPostcode.new(address.scan(REGEX).last[0])
     where(name: postcode.norm).first
   end
 
