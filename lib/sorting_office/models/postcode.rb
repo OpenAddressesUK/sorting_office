8d8be4124dc720894e072d87318dfec8839ba4a4
pezholio <pezholio@gmail.com>
2015-01-16 17:06:29 +0000

    Fix bad regex

--- lib/sorting_office/models/postcode.rb
+++ lib/sorting_office/models/postcode.rb
@@ -1,6 +1,6 @@
 class Postcode
 
-  REGEX = /([A-PR-UWYZ01][A-Z01]?[0-9IO][0-9A-HJKMNPR-YIO]\s?[0-9IO][ABD-HJLNPQ-Z10]{2})/i
+  REGEX = /([A-PR-UWYZ01][A-Z01]?[0-9IO][0-9A-HJKMNPR-YIO]?\s?[0-9IO][ABD-HJLNPQ-Z10]{2})/i
 
   def self.calculate(address)
     postcode = UKPostcode.new(address.match(REGEX)[0])
